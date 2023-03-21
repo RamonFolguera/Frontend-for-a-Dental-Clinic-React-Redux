284194606a85bccd4a560c7185740537a0dfe9c7
Ramón Folguera <53578007+brannell@users.noreply.github.com>
2023-03-21 21:49:02 +0100

    feat: added appointments function to get all appointments as patient

--- happy-teeth/src/layout/05-AppointmentsAsClient/AppointmentsAsClient.jsx
+++ happy-teeth/src/layout/05-AppointmentsAsClient/AppointmentsAsClient.jsx
@@ -1,7 +1,77 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Spinner } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { bringAppointments } from '../../services/apiCalls';
+import { userData } from '../userSlice';
 
 export const AppointmentsAsClient = () => {
+
+  const [appointment, setAppointment] = useState([]);
+
+  const credentialsRdx = useSelector(userData)
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+
+  useEffect(() => {
+
+    if(appointment.length === 0){
+      bringAppointments(credentialsRdx.credentials.token)
+        .then(
+          result => {
+            console.log(result)
+  
+            setAppointment(result.data.data)
+          }
+        )
+        .catch(error => console.log(error))
+    }
+
+  },[appointment])
+
+  // const selected = (user) => {
+    
+  //   dispatch(addChoosen({ choosenOject: user}))
+  //   console.log({ choosenOject: user})
+  //   setTimeout(() => {
+  //     navigate('/profile');
+  //   }, 500)
+  // }
+
+
+
   return (
-    <div>AppointmentsAsClient</div>
+    <div>AppointmentsAsClient
+
+
+{ appointment.length > 0 ? 
+      (<div className="cardsContainer">
+        {
+          appointment.map(
+            info => {
+              return (
+          
+                  <div
+                    className= "userCardDesign"
+                    key={info.id}>
+                    {info.date}
+                    Doctor: {info.Doctor.User.name}
+                  </div>
+             
+              )
+            }
+          )
+
+        }  
+        </div>)
+        
+        :
+
+        ( <Spinner animation="border" variant="primary" />)
+      
+      }
+    </div>
   )
 }
--- happy-teeth/src/services/apiCalls.js
+++ happy-teeth/src/services/apiCalls.js
@@ -10,8 +10,16 @@ export const bringUsers = async (token) => {
         'Authorization': 'Bearer '+ token,  
       }
     };
-
     return await axios.get(`${root}users/admin`, config);
+}
+
+export const bringAppointments = async (token) => {
+  let config = {
+    headers: { 
+      'Authorization': 'Bearer '+ token,  
+    }
+  };
+  return await axios.get(`${root}appointments/user`, config);
 }
 
 export const registerUser = async (body) => {
@@ -24,4 +32,13 @@ export const logMe = async (body) => {
 
     return await axios.post(`${root}auth/login`, body)
 
+}
+
+export const bookAppointment = async (body, token) => {
+  let config = {
+    headers: { 
+      'Authorization': 'Bearer '+ token,  
+    }
+  };
+  return await axios.post(`${root}appointments`, body, config )
 }
