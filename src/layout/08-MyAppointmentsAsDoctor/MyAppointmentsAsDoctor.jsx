5fd4bdc1940003c3a6ca6229fb84157c92591ef4
Ramón Folguera <53578007+brannell@users.noreply.github.com>
2023-03-23 23:06:09 +0100

    feat: added function to get only my appointments loggedin as a dentist

--- src/layout/08-MyAppointmentsAsDoctor/MyAppointmentsAsDoctor.jsx
+++ src/layout/08-MyAppointmentsAsDoctor/MyAppointmentsAsDoctor.jsx
@@ -1,7 +1,66 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Spinner } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+import { bringMyAppointmentsAsDoctor } from '../../services/apiCalls';
+import { userData } from '../userSlice';
 
-export const PatientsProfile = () => {
+export const MyAppointmentsAsDoctor = () => {
+  
+  const [appointments, setAppointments] = useState([]);
+
+  const credentialsRdx = useSelector(userData)
+  
+  useEffect(() => {
+  if(appointments.length === 0){
+    bringMyAppointmentsAsDoctor(credentialsRdx.credentials.token)
+      .then(
+        result => {
+          console.log(result.data.data)
+          
+          setAppointments(result.data.data)
+        }
+      )
+      .catch(error => console.log(error))
+  }
+
+},[appointments])
+  
+  
+  
+  
+  
   return (
-    <div>PatientsProfile</div>
+    <div>My appointments as doctor
+
+{ appointments.length > 0 ? 
+      (<div className="cardsContainer">
+        {
+          appointments.map(
+            appointment => {
+              return (
+                  <div
+                    onClick = {() => appointmentSelected(appointment)}
+                    className= "userCardDesign"
+                    key={appointment.id}>
+                      <div>{appointment.date}</div>
+                      <div> {appointment.id}</div>
+                      <div> {appointment.Service.name}</div>
+                    
+                   
+                
+                  </div>
+              )
+            }
+          )
+
+        }  
+        </div>)
+        
+        :
+
+        ( <Spinner animation="border" variant="primary" />)
+      
+      }
+    </div>
   )
 }
